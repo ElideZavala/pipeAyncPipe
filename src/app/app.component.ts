59df27f90427d387de85e08e32cdcf2bae328060
Elide Zavala <zavalavinagreelide@gmail.com>
2023-01-14 22:12:20 -0600

    pipe async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { Observable } from "rxjs"
+import { Subscription } from "rxjs"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'pipeAyncPipe';
+  promiseData!: string;
+  // observableData!: string;
+  // observableSubs: Subscription;
+  promiseObj!: Promise<string>;
+  observableObj!: Observable<string>;
+
+  getPromise() {
+    return new Promise<string>( function( resolve, reject ) {
+      setTimeout(() => {
+        resolve('Timer1 finalizado');
+      }, 2000)
+    })
+  }
+
+  getObservable() {
+    return new Observable<string>((observer: { next: (arg0: string) => void; }) => {
+      setTimeout(() => {
+        observer.next('Timer2 finalizado');
+      }, 4000);
+    })
+  }
+
+  constructor() {
+    // this.getPromise().then(( v: string ) => this.promiseData = v);
+    // this.observableSubs = this.getObservable()
+                                  // .subscribe( ( v: string ) => this.observableData = v);
+
+   this.promiseObj = this.getPromise();
+   this.observableObj = this.getObservable();
+  }
+
+
 }
